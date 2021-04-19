d47ea5da2d6b414e8345d82f5ade908143cfcb70
AGonz123 <32293916+AGonz123@users.noreply.github.com>
2021-04-18 17:47:50 -0700

    Update script.js

--- COVID-Test/COVID-Test/test/script.js
+++ COVID-Test/COVID-Test/test/script.js
@@ -1,11 +1,14 @@
 const $icon = $('.menu-btn');
 const $menu = $('.overlay');
+const $data = $('.container2');
 
 $icon.on('click', function(){
   if(!$menu.hasClass('active')) {
  		$menu.fadeIn().toggleClass('active');
+    $data.fadeOut().removeClass('active');
   } else {
     $menu.fadeOut().removeClass('active');
+    $data.fadeIn().removeClass('active');
   }
 });
 
@@ -15,13 +18,23 @@ var Form1 = document.getElementById("Form1");
     var Next = document.getElementById("Next");
     var Back = document.getElementById("Back");
 
+    var progress = document.getElementById("progress");
+    var submit = document.getElementById("Submit");
+
     Next.onclick = function(){
       Form1.style.left = "-450px";
       Form2.style.left = "40px";
+      progress.style.width = "266.66px";
+      
     }
 
     Back.onclick = function(){
       Form1.style.left = "40px";
       Form2.style.left = "450px";
+      progress.style.width = "133.33px";
     }
 
+    submit.onclick = function(){
+      progress.style.width = "400px";
+
+    }
