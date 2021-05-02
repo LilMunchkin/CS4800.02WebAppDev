9d4d0e34ff14668b8691366226fea8c6073cbacd
cesarm2 <cesarmartinez436@gmail.com>
2021-05-02 13:58:25 -0700

    Add files via upload

---
+++ CovidVue/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <my-Home/>
+  <my-form/>
+</template>
+
+<script setup>
+import MyForm from './components/MyForm.vue'
+import Home from './components/Home.vue'
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
