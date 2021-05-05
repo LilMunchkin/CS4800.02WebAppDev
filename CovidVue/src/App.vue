d8d2fd8f049171f7aefa4021d23624a1d62756fd
cesarm2 <cesarmartinez436@gmail.com>
2021-05-04 21:41:02 -0700

    Add files via upload

---
+++ CovidVue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- <router-link to="/Home">Home</router-link>> -->
+  <!--router-link to="/MyForm">MyForm</router-link-->
+  <router-view />
+  <my-Home/>
+  <my-form/>
+</template>
+
+<script setup>
+
+import Home from './views/Home.vue'
+import MyForm from './views/MyForm.vue'
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
